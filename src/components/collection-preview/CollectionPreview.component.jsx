ce3e2fcd9a5497b26e4a15329d7c8c86de6abc9b
frostz <aidenfrostbite@gmail.com>
2021-01-21 09:34:53 +0530

    Created ShopPage and and created corresponding components

--- src/components/collection-preview/CollectionPreview.component.jsx
+++ src/components/collection-preview/CollectionPreview.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CollectionItem from '../collection-item/CollectionItem.component';
+
 import './CollectionPreview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => {
@@ -9,8 +11,8 @@ const CollectionPreview = ({ title, items }) => {
       <div className="preview">
         {items
           .filter((item, index) => index < 4)
-          .map((item) => (
-            <div key={item.id}>{item.name}</div>
+          .map(({ id, ...itemProps }) => (
+            <CollectionItem key={id} {...itemProps} />
           ))}
       </div>
     </div>
