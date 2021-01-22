2178856bc050afd8f4ffdca0facd1caaf487ac0c
frostz <aidenfrostbite@gmail.com>
2021-01-22 10:22:33 +0530

    Implemented firebase and firestore

--- src/components/sign-in/SignIn.component.jsx
+++ src/components/sign-in/SignIn.component.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import FormInput from '../form-input/FormInput.component';
 import CustomButton from '../custom-button/CustomButton.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './SignIn.styles.scss';
 
@@ -16,9 +16,17 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState({ email: '', password: '' });
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   handleChange = (event) => {
