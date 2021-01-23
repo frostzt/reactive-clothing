8834cebc759757c55d31b7076a65de7d5cfff2f4
frostzt <aidenfrostbite@gmail.com>
2021-01-23 11:52:06 +0530

    Implemented cart functionality

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
-import { TOGGLE_CART_HIDDEN } from './cart.types.js';
+import { TOGGLE_CART_HIDDEN, ADD_ITEM } from './cart.types.js';
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,6 +11,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         hidden: !state.hidden,
+      };
+
+    case ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload],
       };
 
     default:
