dd43cda7e99e92d6319b8f004f4aa64a4284ac74
frostzt <aidenfrostbite@gmail.com>
2021-01-24 10:56:35 +0530

    Implemented adding items to cart and updated them to store quantity

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,15 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItems = cartItems.find(
+    (item) => item.id === cartItemToAdd.id
+  );
+
+  if (existingCartItems) {
+    return cartItems.map((item) =>
+      item.id === cartItemToAdd.id
+        ? { ...item, quantity: item.quantity + 1 }
+        : item
+    );
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
