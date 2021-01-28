b0afd0431f7e11dc68fbabec2b11580fe0c723d7
frostzt <aidenfrostbite@gmail.com>
2021-01-28 20:45:37 +0530

    Implementing store persistance

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './rootReducer';
 
 const middlewares = [logger];
 
-const store = createStore(
+export const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middlewares))
 );
+
+export const persistor = persistStore(store);
 
 export default store;
