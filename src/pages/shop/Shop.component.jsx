fac46ab13703b383eaf6704d2a824db347fba10c
frostzt <aidenfrostbite@gmail.com>
2021-02-04 11:16:51 +0530

    Implemented routing on the landing page

--- src/pages/shop/Shop.component.jsx
+++ src/pages/shop/Shop.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import CollectionOverview from '../../components/collections-overview/CollectionsOverview.component';
+import CollectionPage from '../collection/Collection.component';
 
-const ShopPage = ({ collections }) => {
+const ShopPage = ({ match }) => {
   return (
     <div>
-      <CollectionOverview />
+      <Route exact path={`${match.path}`} component={CollectionOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
