ca9495e4581b8bf4ccaf1ce6529c070a569ba65b
frostzt <aidenfrostbite@gmail.com>
2021-02-08 14:13:22 +0530

    Implemented redux-saga for thunks

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,32 @@
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import {
+  firestore,
+  convertCollectionSnapshotToMap,
+} from '../../firebase/firebase.utils';
+
+import {
+  FETCH_COLLECTIONS_START,
+  FETCH_COLLECTIONS_FAILURE,
+  FETCH_COLLECTIONS_SUCCESS,
+} from './shop.types';
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from './shop.actions';
+
+export function* fetchCollectionsAsync() {
+  try {
+    const collectionRef = firestore.collection('collection');
+    const snapshot = yield collectionRef.get();
+    const collectionMap = yield call(convertCollectionSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSuccess(collectionMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(FETCH_COLLECTIONS_START, fetchCollectionsAsync);
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
+
+import { fetchCollectionsStart } from './shop/shop.sagas';
 
 import rootReducer from './rootReducer';
 
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
@@ -18,6 +22,8 @@ export const store = createStore(
     ? composeWithDevTools(applyMiddleware(...middlewares))
     : applyMiddleware(...middlewares)
 );
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 export const persistor = persistStore(store);
 
